793a8034a0f694dc28dce22f828e14587219a23f
Chi Nguyen <chinh@dipro.vn>
2022-12-03 17:59:53 +0700

    build: build source base

---
+++ src/components/pages/Layout/index.tsx
@@ -0,0 +1,18 @@
+import { ReactNode } from 'react';
+
+import Footer from 'components/organisms/Footer';
+import Header from 'components/organisms/Header/Header';
+
+type Props = {
+  children: ReactNode;
+};
+
+export default function Layout({ children }: Props) {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="grow">{children}</main>
+      <Footer />
+    </div>
+  );
+}
