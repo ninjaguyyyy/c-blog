33b87c716399e4f9be96cdadf05ea372bbcc5f30
Chi Nguyen <chinh@dipro.vn>
2022-12-24 22:22:01 +0700

    feat: implement header and footer

--- src/components/organisms/Footer/index.tsx
+++ src/components/organisms/Footer/index.tsx
@@ -1,5 +1,18 @@
-import * as React from 'react';
+import Copyright from 'components/molecules/Copyright';
+import SubscribeForm from 'components/molecules/SubscribeForm';
+
+import './index.scss';
 
 export default function Footer() {
-  return <div>Footer efsdf</div>;
+  return (
+    <div className="py-14 footer flex flex-col justify-center items-center xs:py-8 xs:px-6">
+      <p className="text-center text-4xl font-bold mb-8 xs:text-2xl xs:w-1/2">
+        Subscribe to our Newsletter
+      </p>
+      <div className="w-1/3 xs:w-full">
+        <SubscribeForm />
+        <Copyright />
+      </div>
+    </div>
+  );
 }
