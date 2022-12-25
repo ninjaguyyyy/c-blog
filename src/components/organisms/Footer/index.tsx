026b1c65cce041040137042fa5b56867277d0e50
trongpham <phamvantrong7.qn@gmail.com>
2022-12-25 15:58:26 +0700

    fix docker compose

--- src/components/organisms/Footer/index.tsx
+++ src/components/organisms/Footer/index.tsx
@@ -7,7 +7,7 @@ export default function Footer() {
   return (
     <div className="py-14 footer flex flex-col justify-center items-center xs:py-8 xs:px-6">
       <p className="text-center text-4xl font-bold mb-8 xs:text-2xl xs:w-1/2">
-        Subscribe to our Newsletter
+        Subscribe to our Newsletterrrr
       </p>
       <div className="w-1/3 xs:w-full">
         <SubscribeForm />
